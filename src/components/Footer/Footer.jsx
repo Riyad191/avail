4d6e15eca6cc94479d91ff3d7bb2f589b4d9492f
Riyadh Alsalemi <riyadh@Riyadhs-MBP.lan>
2023-03-02 10:28:16 -0600

    add-feature

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Stack, Typography } from "@mui/material";
 import { FooterBox, LogoBox } from "./styles";
-import footerLogo from "../../images/walmart-logo.png";
 
 const Footer = () => {
   return (
@@ -11,10 +10,7 @@ const Footer = () => {
         sx={{ justifyContent: "center", alignItems: "center" }}
       ></Stack>
       <LogoBox>
-        <img src={footerLogo} height="34" />
-        <Typography variant="subtitle2">
-          2022 Built by Omni Supply Chain System
-        </Typography>
+        <Typography variant="subtitle2">Availability Dashboard</Typography>
       </LogoBox>
     </FooterBox>
   );
